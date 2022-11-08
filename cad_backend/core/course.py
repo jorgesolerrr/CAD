26c2e1dea80c1a1b287dcfa947ef2d836f97ac64
jorgesolerrr <jorgeasoler2015@gmail.com>
2022-11-07 19:32:09 -0500

    little changes

--- cad_backend/core/course.py
+++ cad_backend/core/course.py
@@ -1,8 +1,13 @@
-from dataclasses import dataclass
 from moodle_conn.moodle import Moodle 
 from core.schemas import *
 
 
 class BaseCourse(Moodle):
-    def create_course(course : Course):
-        course.
+    def create_course(self, course : Course):
+        data = {
+            "courses[0][fullname]" : course.fullname,
+            "courses[0][shortname]" : course.shortname,
+            "courses[0][categoryid]" : course.categoryid,
+            "courses[0][idnumber]" : course.idnumber
+        }
+        
